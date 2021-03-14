8a6d84df7ab437b6ca80c722a1ebe4087e6bfd80
Marcel Judth <42699984+marcel-judth@users.noreply.github.com>
2021-03-14 19:56:52 +0100

    added tickets and people section

--- index.js
+++ index.js
@@ -25,3 +25,22 @@ VisSense.VisMon.Builder(VisSense(myVideo))
   })
   .build()
   .start();
+
+function onPersonSelect(e) {
+  console.log(e.target);
+  const persons = document.querySelectorAll('.person');
+  const textWrapper = document.querySelectorAll('.person-text-wrapper');
+  const id = e.target.id;
+  const textElement = document.querySelector('.' + id + '-text');
+
+  persons.forEach((e) => {
+    e.classList.remove('active');
+  });
+
+  textWrapper.forEach((e) => {
+    e.classList.remove('active');
+  });
+
+  e.target.classList.add('active');
+  textElement.classList.add('active');
+}
